197c536be770b72e7dc7146568058fc24f602e5d
Matheus <joaosobramatheus@gmail.com>
2025-11-20 23:20:07 -0300

    <feat> Adiciona na classe Carrinho a adição ao mesmo

--- vendas/src/main/java/br/icev/vendas/Carrinho.java
+++ vendas/src/main/java/br/icev/vendas/Carrinho.java
@@ -1,14 +1,30 @@
 package vendas.src.main.java.br.icev.vendas;
 
 
-import vendas.src.main.java.br.icev.vendas.Produto;
 import vendas.src.main.java.br.icev.vendas.excecoes.QuantidadeInvalidaException;
 
 import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Carrinho {
+
+    private Map<String, Produto> produtos;
+    private Map<String, Integer> quantidades;
+
+    public Carrinho() {
+        this.produtos = new HashMap<>();
+        this.quantidades = new HashMap<>();
+    }
+
     public void adicionar(Produto produto, int quantidade) throws QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade deve ser maior que zero");
+        }
+
+        String codigo = produto.getCodigo();
+        produtos.put(codigo, produto);
+        quantidades.put(codigo, quantidades.getOrDefault(codigo, 0) + quantidade);
     }
 
     public BigDecimal getSubtotal() {
