eb23242f5a86a3486caf16379fb1455a9a3bc952
Matheus <joaosobramatheus@gmail.com>
2025-11-20 23:05:04 -0300

    <feat> Adiciona na classe Estoque a adição

--- vendas/src/main/java/br/icev/vendas/Estoque.java
+++ vendas/src/main/java/br/icev/vendas/Estoque.java
@@ -4,9 +4,21 @@ package vendas.src.main.java.br.icev.vendas;
 import vendas.src.main.java.br.icev.vendas.excecoes.QuantidadeInvalidaException;
 import vendas.src.main.java.br.icev.vendas.excecoes.SemEstoqueException;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Estoque {
+    private Map<String, Integer> produtos;
+
+    public Estoque() {
+        this.produtos = new HashMap<>();
+    }
+
     public void adicionarEstoque(String codigo, int quantidade) throws QuantidadeInvalidaException {
-        throw new UnsupportedOperationException("TODO");
+        if (quantidade <= 0) {
+            throw new QuantidadeInvalidaException("Quantidade deve ser maior que zero");
+        }
+        produtos.put(codigo, produtos.getOrDefault(codigo, 0) + quantidade);
     }
 
     public int getDisponivel(String codigo) {
